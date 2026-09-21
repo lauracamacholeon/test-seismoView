542007a77f248008cff06dd10a85b21d3d0dec77
laura camacho <lauracamacholeon@gmail.com>
2026-09-21 12:52:14 -0500

    feat(core): add application shell layout

---
+++ src/app/core/layout/shell/shell.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'sv-shell',
+  styleUrl: './shell.scss',
+  templateUrl: './shell.html',
+})
+export class Shell {}
