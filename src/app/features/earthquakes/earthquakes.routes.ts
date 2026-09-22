15e29b94c5de1b3ee2b8d20dae6b050e5e57b99f
laura camacho <lauracamacholeon@gmail.com>
2026-09-21 21:47:36 -0500

    feat(map): register the maplibre provider in the earthquakes route

--- src/app/features/earthquakes/earthquakes.routes.ts
+++ src/app/features/earthquakes/earthquakes.routes.ts
@@ -4,11 +4,16 @@ import { provideState } from '@ngrx/store';
 
 import * as earthquakesEffects from './data-access/state/earthquakes.effects';
 import { earthquakesFeature } from './data-access/state/earthquakes.reducer';
+import { mapFactoryProvider } from './map/map-factory.provider';
 
 export const EARTHQUAKES_ROUTES: Routes = [
   {
     path: '',
-    providers: [provideState(earthquakesFeature), provideEffects(earthquakesEffects)],
+    providers: [
+      provideState(earthquakesFeature),
+      provideEffects(earthquakesEffects),
+      mapFactoryProvider,
+    ],
     loadComponent: () =>
       import('./containers/earthquake-viewer/earthquake-viewer').then((m) => m.EarthquakeViewer),
   },
